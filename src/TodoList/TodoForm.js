eca029d8f24abd9fbf367032c023b08533387e0c
saman-zdf <samanfyz68@gmail.com>
2021-10-29 09:29:55 +0800

    Form has been added, with post request we can create a task

--- src/TodoList/TodoForm.js
+++ src/TodoList/TodoForm.js
@@ -1,11 +1,60 @@
-import { useState } from 'react';
+import React, { Component } from 'react';
+import TextField from '@mui/material/TextField';
+import { Button } from '@mui/material';
 
-const TodoForm = () => {
-  return (
-    <>
-      <form></form>
-    </>
-  );
-};
+class TodoForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      api_url: props.api_url,
+      task: '',
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleTaskChange = this.handleTaskChange.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.formSubmit(e.target);
+  }
+
+  async formSubmit(formData) {
+    let data = new FormData(formData);
+    await fetch(this.state.api_url, {
+      method: 'POST',
+      mode: 'cors',
+      body: data,
+    })
+      .then((res) => res.json())
+      .then((data) => this.props.updateTodoList(data));
+  }
+  handleTaskChange(e) {
+    this.setState({
+      task: e.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <form onSubmit={this.handleSubmit} id='todo-form' autoComplete='off'>
+          <TextField
+            id='task-input'
+            label='Task Description'
+            variant='outlined'
+            type='text'
+            name='todo[task]'
+            onChange={this.handleTaskChange}
+          />
+          <br />
+          <Button variant='contained' color='primary' type='submit'>
+            Add Task
+          </Button>
+        </form>
+      </>
+    );
+  }
+}
 
 export default TodoForm;
--- src/TodoList/TodoList.js
+++ src/TodoList/TodoList.js
@@ -1,26 +1,52 @@
 import React, { Component, useState, useEffect } from 'react';
 import TodoForm from './TodoForm';
+import TodoItem from '../components/TodoItem';
 const url = 'http://127.0.0.1:3001/api/v1/todos';
 
-const TodoList = () => {
-  const [todo, setTodo] = useState([]);
-  const fetchData = async () => {
-    let response = await fetch(url);
-    let data = await response.json();
-    setTodo(data);
-  };
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+    };
+    this.updateTodoList = this.updateTodoList.bind(this);
+  }
+  componentDidMount() {
+    this.getTask();
+  }
 
-  useEffect(() => {
-    fetchData();
-  }, []);
-  return (
-    <>
-      <TodoForm />
-      {todo.map((item) => {
-        return <p key={item.id}>{item.task}</p>;
-      })}
-    </>
-  );
-};
+  getTask() {
+    fetch(url)
+      .then((res) => res.json())
+      .then((response_items) => {
+        this.setState({
+          items: response_items.reverse(),
+        });
+      });
+  }
+
+  updateTodoList(item) {
+    let _items = this.state.items;
+    // unshift adds to the beginning of array
+    _items.unshift(item);
+    this.setState({
+      items: _items,
+    });
+  }
+
+  render() {
+    console.log(this.state.items);
+    return (
+      <div>
+        <TodoForm api_url={url} updateTodoList={this.updateTodoList} />
+        <ul id='todo_list'>
+          {this.state.items.map((item) => {
+            return <TodoItem key={item.id} item={item} />;
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default TodoList;
